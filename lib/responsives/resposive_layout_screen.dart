8d9bb71363966930a628504fc76505fd09474a86
krishnasharmak05 <146259706+krishnasharmak05@users.noreply.github.com>
2024-05-27 18:47:12 +0530

    Add responsive layouts for web and mobile

---
+++ lib/responsives/resposive_layout_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:insta/utils/dimensions.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget webScreenLayout;
+  final Widget mobileScreenLayout;
+  const ResponsiveLayout(
+      {super.key,
+      required this.mobileScreenLayout,
+      required this.webScreenLayout});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > webScreenSize) {
+          return webScreenLayout;
+        } else {
+          return mobileScreenLayout;
+        }
+      },
+    );
+  }
+}
