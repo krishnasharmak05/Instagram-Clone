f42bfbe30fd48a7b9a0516b8cf7c0e41d787b13a
krishnasharmak05 <krishnasharmak05@protonmail.com>
2024-05-29 00:36:19 +0530

    Add Comments to explain, in dimensions and responsive_layout_screen

--- lib/responsives/resposive_layout_screen.dart
+++ lib/responsives/resposive_layout_screen.dart
@@ -13,7 +13,10 @@ class ResponsiveLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        if (constraints.maxWidth > webScreenSize && constraints.maxHeight > webScreenSize) {
+        if (constraints.maxWidth > webScreenSize &&
+            constraints.maxHeight > webScreenSize) {
+          // Added the height part to prevent mobile screen from changing to webScreen when rotated to Landscape mode.
+          // We can't do anything about the case where the height of the web screen is smaller than webScreenSize :sad:z  .
           return webScreenLayout;
         } else {
           return mobileScreenLayout;
--- lib/utils/dimensions.dart
+++ lib/utils/dimensions.dart
@@ -1,3 +1,4 @@
-import 'package:flutter/material.dart';
 
-const webScreenSize = 600;
+
+const webScreenSize =
+    400; // Originally at 600. I have set it to 400 to because that way I can't set a browser to open the mobile screen, and mobiles can't open the web-view.
