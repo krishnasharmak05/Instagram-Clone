5c3e9d0b2e4a5e2fdd3774ebce9e3dc903d73a0c
krishnasharmak05 <krishnasharmak05@protonmail.com>
2024-05-29 00:38:19 +0530

    Initialize Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:insta/pages/home.dart';
+import 'package:insta/secrets.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: apiKey,
+        appId: appId,
+        messagingSenderId: messagingSenderId,
+        projectId: projectId,
+        storageBucket: storageBucket,
+      ),
+    );
+  } else {
+    Firebase.initializeApp();
+  }
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   runApp(const HomePage());
 }
