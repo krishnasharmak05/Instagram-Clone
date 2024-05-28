2dd6e240da6ed18b6b3798ec4046b6db7b4ecbf6
krishnasharmak05 <krishnasharmak05@protonmail.com>
2024-05-29 01:31:06 +0530

    Fix: Firebase issue in running on Android Fixed.

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:insta/firebase_options.dart';
 import 'package:insta/pages/home.dart';
 import 'package:insta/secrets.dart'; // Unavailable on GitHub to prevent unauthorized access
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   if (kIsWeb) {
-    Firebase.initializeApp(
+    await Firebase.initializeApp(
       options: const FirebaseOptions(
         apiKey: apiKey,
         appId: appId,
@@ -18,7 +19,10 @@ void main() {
       ), // Add your own API key from https://console.firebase.google.com before execution.
     );
   } else {
-    Firebase.initializeApp();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions
+          .currentPlatform, // Set up your own firebase_option.dart file using flutterfire_cli for this line to work
+    );
   }
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   runApp(const HomePage());
